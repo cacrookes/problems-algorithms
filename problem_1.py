0967be4d2913a9874b4f8b730175151de7cc9d75
Christopher Crookes <cacrookes@gmail.com>
2019-05-07 22:00:22 -0700

    Fixed PEP8 issues.

--- problem_1.py
+++ problem_1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def sqrt(number):
     """
     Calculate the floored square root of a number
@@ -29,8 +30,8 @@ def sqrt(number):
     return root
 
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
